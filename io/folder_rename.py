c47711714db88a77aec45b2a9117cecc73e81a8e
Kevin Lin <lin12512@gmail.com>
2017-07-28 11:00:59 +0800

    fix the re-definition of dict

--- io/folder_rename.py
+++ io/folder_rename.py
@@ -1,9 +1,9 @@
-import sys
 import os
 import json
 import argparse
 
 #dict = {'drawable-mdpi': 'scale-100', "drawable-hdpi": "scale-150", "drawable-xhdpi": "scale-200", "drawable-xxhdpi": "scale-300", "drawable-xxxhdpi": "scale-400"}
+
 
 def rename(old, new):
   try:
@@ -11,22 +11,24 @@ def rename(old, new):
   except:
     pass
 
+
 def travel(basedir):
   for fn in os.listdir(basedir):
     curdir = os.path.join(basedir, fn)
     if not os.path.isdir(curdir):
       continue
-    travel(curdir);
-    for k, v in dict.iteritems():
+    travel(curdir)
+    for k, v in dt.iteritems():
       old = os.path.join(basedir, k)
       new = os.path.join(basedir, v)
       rename(old, new)
-      
+
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
 
-  parser.add_argument("-f", "--config", help="config file that stores key pairs")
+  parser.add_argument(
+      "-f", "--config", help="config file that stores key pairs")
   parser.add_argument("-d", "--dir", help="base dir")
 
   args = parser.parse_args()
@@ -39,10 +41,10 @@ if __name__ == "__main__":
   if args.config:
     config = args.config
 
-  try:  
-    js=open(config)
-    dict=json.load(js)
-  except Exception, ex:
+  try:
+    js = open(config)
+    dt = json.load(js)
+  except IOError, ex:
     print ex
-    
+
   travel(basedir)
